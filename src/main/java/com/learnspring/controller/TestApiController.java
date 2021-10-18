bec76ace98f893e0d3ad43537267b55eae27ae63
reema117 <86834133+reema117@users.noreply.github.com>
2021-10-18 09:22:06 +0530

    Update TestApiController.java

--- src/main/java/com/learnspring/controller/TestApiController.java
+++ src/main/java/com/learnspring/controller/TestApiController.java
@@ -7,12 +7,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.learnspring.model.EmployeeDetails;
+import com.learnspring.model.FetchEmployeeDetails;
 import com.learnspring.model.Status;
 import com.learnspring.service.ApiService;
+
 
 @RestController 
 @CrossOrigin("*")//it will take any domain 
@@ -29,8 +33,11 @@ public class TestApiController {
 		return new 	ResponseEntity<>(status,HttpStatus.OK);
 				}
 	@PostMapping(value = "/getDetails",  consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<EmployeeDetails> employeeDetails(){
-		EmployeeDetails employeeDetails = apiService.apiEmployee();
+	public ResponseEntity<EmployeeDetails> employeeDetails(
+	@RequestBody FetchEmployeeDetails fetchEmployeeDetails
+			)
+	{
+		EmployeeDetails employeeDetails = apiService.apiEmployee(fetchEmployeeDetails);
 		
 		return new 	ResponseEntity<>(employeeDetails,HttpStatus.OK);
 		
